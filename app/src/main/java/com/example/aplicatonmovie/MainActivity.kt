75200c8ddb9eb153d837b94fdd702dce4a4e8514
sawaliyah <sawaliah99@gmail.com>
2022-11-15 13:19:29 +0700

    sawaliyah : intent pada halaman login dan membuat buttom navigation (activity dan fragment)

--- app/src/main/java/com/example/aplicatonmovie/MainActivity.kt
+++ app/src/main/java/com/example/aplicatonmovie/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.aplicatonmovie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var loginButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginButton = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener(){
+            val intent = Intent(this@MainActivity,MainActivity2::class.java)
+            startActivity(intent)
+        }
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/aplicatonmovie/MainActivity2.kt
@@ -0,0 +1,61 @@
+package com.example.aplicatonmovie
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.aplicatonmovie.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityMain2Binding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main2)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.menu1 ->{
+                Toast.makeText(this,"Not Available yet", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.menu2 ->{
+                Toast.makeText(this,"Not Available yet", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.menu3 ->{
+                Toast.makeText(this,"Not Available yet", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.menu4 ->{
+                Toast.makeText(this,"Not Available yet", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> return true
+        }
+    }
+}
